6b0d58f814eef7d04a00fc9a6332b144f84b78e0
M B <mablasteam@gmail.com>
2023-03-24 23:11:23 +0100

    added cargo support

---
+++ src/Managers/Cargo.ts
@@ -0,0 +1,40 @@
+import { Module } from "./Module.ts";
+import * as Toml from "https://deno.land/std@0.180.0/encoding/toml.ts";
+
+export async function crawlCargoLock(path: string, requestCargoApi = true): Promise<Module[]> {
+    const modules: Module[] = []
+
+    const cargoLockRaw = await Deno.readTextFile(path)
+    const cargoLock = Toml.parse(cargoLockRaw)
+    if (typeof cargoLock.package !== "object" && cargoLock.package) return []
+    const pack = cargoLock.package as Array<object>;
+    for (let i = 0; i < pack.length; i++) { 
+        if(!requestCargoApi) modules.push(pack[i] as Module)
+        else {
+            const mod_base = pack[i] as Module
+            if(!mod_base.name) continue;
+            const response = await fetch("https://crates.io/api/v1/crates/"+mod_base.name)
+            if(!response.ok) {
+                modules.push(pack[i] as Module)
+                continue
+            }
+            const json = await response.json()
+            const crate = json.crate;
+            if(!crate) {
+                modules.push(pack[i] as Module)
+                continue
+            }
+            const mod: Module = {}
+            if(crate.name)          mod.name            = crate.name.toString()
+            if(crate.description)   mod.description     = crate.description.toString()
+            if(crate.documentation) mod.documentation   = crate.documentation.toString()
+            if(crate.homepage)      mod.homepage        = crate.homepage.toString()
+            if(crate.license)       mod.license         = crate.license.toString()
+            if(crate.authors)       mod.author          = crate.authors.toString()
+            if(crate.repository)    mod.repo            = crate.repository.toString()
+            modules.push(mod)
+        }
+    }
+
+    return modules;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { readConfig } from "./config/ReadConfig.ts";
 import * as FS from "https://deno.land/std@0.181.0/fs/mod.ts";
 import { crawlNodeModules } from "./Managers/Npm.ts";
 import { Module } from "./Managers/Module.ts";
+import { crawlCargoLock } from "./Managers/Cargo.ts";
 
 const config = readConfig()!
 
@@ -11,10 +12,10 @@ for await (const entry of FS.walk(".")) {
     switch (entry.name) {
         case "node_modules":
             if(!config.exclude.includes(entry.path) && entry.isDirectory) Array.prototype.push.apply(modules, await crawlNodeModules(entry.path))
-
+            break;
+        case "Cargo.lock":
+            if(!config.exclude.includes(entry.path) && entry.isFile) Array.prototype.push.apply(modules, await crawlCargoLock(entry.path))
             break;
     }
 }
 console.log(modules)
-
-
