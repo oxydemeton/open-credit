cb3b24a9c542da6b95aefc00fe57470ea79b8ba2
M B <mablasteam@gmail.com>
2023-03-24 23:28:13 +0100

    made managers optional

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -3,6 +3,6 @@ export interface Config {
     exclude: string[]                       //Folders to exclude
     json_report: string | undefined         //File to store a details in json format
     allow_api_calls: boolean                //If deno is allowed to request apis to get additional information about packages
-    managers: Array<string>                 //Select which managers should be searched for
+    managers?: Array<string>                 //Select which managers should be searched for
 }
 
