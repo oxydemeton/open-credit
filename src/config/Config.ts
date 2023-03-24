2ddaee53094c70048f132cabea06faefb5b577ba
M B <mablasteam@gmail.com>
2023-03-24 23:24:07 +0100

    add manager config

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,7 +1,8 @@
 export interface Config {
     output: string                          //File to output all contributors in
     exclude: string[]                       //Folders to exclude
-    json_report: string | undefined         //File to store a detailes in json format
+    json_report: string | undefined         //File to store a details in json format
     allow_api_calls: boolean                //If deno is allowed to request apis to get additional information about packages
+    managers: Array<string>                 //Select which managers should be searched for
 }
 
