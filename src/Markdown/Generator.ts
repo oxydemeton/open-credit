33e6628f9419cfaa5709ba99f9f3528d6a06e943
M B <mablasteam@gmail.com>
2023-03-25 21:17:44 +0100

    splitted by manager

--- src/Markdown/Generator.ts
+++ src/Markdown/Generator.ts
@@ -7,8 +7,10 @@ export function generate(modules: Module[]): string {
     const writeLine = (txt: string) => {
         md += txt + "\n"
     }
+    const split_modules = separateModules(modules)
 
-    modules.forEach((mod) => {
+    writeLine("### Cargo Modules")    
+    split_modules.cargo.forEach((mod) => {
         writeLine("- " + mod.name)
         if (mod.author) writeLine("    - Author: " + mod.author)
         if (mod.version) writeLine("    - Version: " + mod.version)
@@ -20,5 +22,33 @@ export function generate(modules: Module[]): string {
         }
         if (mod.homepage) writeLine("    - Homepage: " + mod.homepage)
     })
+    writeLine("### NPM Modules")
+    split_modules.npm.forEach((mod) => {
+        writeLine("- " + mod.name)
+        if (mod.author) writeLine("    - Author: " + mod.author)
+        if (mod.version) writeLine("    - Version: " + mod.version)
+        if (mod.license) writeLine("    - License: " + mod.license)
+        if (mod.repo) writeLine("    - Repository: " + mod.repo)
+        if (mod.description) writeLine("    - Description: " + mod.description)
+        if (mod.documentation) {
+            writeLine("    - Documentation: " + mod.documentation)
+        }
+    })
     return md
+}
+
+function separateModules(modules: Module[]): {npm: Module[], cargo: Module[]} {
+    const cargoModules: Module[] = []
+    const npmModules: Module[] = []
+    modules.forEach((mod) => {
+        if (mod.manager === "cargo") {
+            cargoModules.push(mod)
+        } else if (mod.manager === "npm") {
+            npmModules.push(mod)
+        }
+    })
+    return {
+        cargo: cargoModules,
+        npm: npmModules,
+    }
 }
