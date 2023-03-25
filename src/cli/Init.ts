1a41b5c56c5b167a8ba8f9f68681923fbde58dc6
M B <mablasteam@gmail.com>
2023-03-25 13:21:38 +0100

    added init and run

---
+++ src/cli/Init.ts
@@ -0,0 +1,5 @@
+import { defaultConfig } from "../config/default.ts"
+
+export async function init(path = "opencredit.jsonc") {
+    await Deno.writeTextFile(path, JSON.stringify(defaultConfig))
+}
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,24 @@ import { readConfig } from "./config/ReadConfig.ts"
 import { generate as mdGenerate } from "./Markdown/Generator.ts"
 import { collectAll } from "./cli/Collect.ts"
 import { parse as parseArgs } from "./cli/Args.ts"
+import { init as initConfigFile } from "./cli/Init.ts"
 
-if (parseArgs() === null) Deno.exit(0)
-const config = readConfig()!
+const args = parseArgs()
+if (args === null) Deno.exit(0)
+if (args[0] === "init") {
+    const path = args[1]?.overwrite_config
+        ? args[1].overwrite_config
+        : "opencredit.jsonc"
+    initConfigFile(path)
+    console.log("Created config file: " + path)
+    Deno.exit(0)
+}
+const config_path = args[1]?.overwrite_config
+    ? args[1]?.overwrite_config
+    : "opencredit.jsonc"
+const config = readConfig(config_path)!
+if (args[1]?.overwrite_json) config.json_report = args[1]?.overwrite_json
+if (args[1]?.overwrite_md) config.output = args[1]?.overwrite_md
 
 //Process folders
 const modules = await collectAll(config)
