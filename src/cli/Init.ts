fa734f115a0b577ba12d7f4cccc157da36ef60f3
M B <mablasteam@gmail.com>
2023-03-26 17:31:23 +0200

    added feedback to init command

--- src/cli/Init.ts
+++ src/cli/Init.ts
@@ -1,5 +1,6 @@
 import { defaultConfig } from "../config/default.ts"
 
 export async function init(path = "opencredit.jsonc") {
+    console.log(`Creating ${path}...`)    
     await Deno.writeTextFile(path, JSON.stringify(defaultConfig, undefined, 4))
 }
