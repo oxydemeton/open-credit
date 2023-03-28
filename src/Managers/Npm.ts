1f6e445ab2a8c7a7e22b7ec3faf09462bcd4d666
M B <mablasteam@gmail.com>
2023-03-28 20:53:39 +0200

    now creating cache if needed

--- src/Managers/Npm.ts
+++ src/Managers/Npm.ts
@@ -1,21 +1,33 @@
 import { walk } from "https://deno.land/std@0.181.0/fs/walk.ts"
 import { Module } from "./Module.ts"
 import { Config } from "../config/Config.ts"
-import * as Path from "https://deno.land/std@0.181.0/path/mod.ts";
-import { NpmCache } from "../cache/NpmCache.ts";
-import * as Crypto from "https://deno.land/std@0.181.0/crypto/crypto.ts";
-import { toHashString } from "https://deno.land/std@0.181.0/crypto/to_hash_string.ts";
+import * as Path from "https://deno.land/std@0.181.0/path/mod.ts"
+import { NpmCache } from "../cache/NpmCache.ts"
+import * as Crypto from "https://deno.land/std@0.181.0/crypto/crypto.ts"
+import { toHashString } from "https://deno.land/std@0.181.0/crypto/to_hash_string.ts"
 
-export async function crawlNodeModules(root: string, config: Config): Promise<Module[]> {
+export async function crawlNodeModules(
+    root: string,
+    config: Config,
+): Promise<Module[]> {
+    if (config.cache) {
+        Deno.mkdirSync(Path.join(config.cache, "npm"), { recursive: true })
+    }
     const modules: Set<Module> = new Set()
     for await (const entry of walk(root)) {
         if (entry.name !== "package.json" || !entry.isFile) continue
 
         const json = JSON.parse(await Deno.readTextFile(entry.path)) as any
         const mod: Module = { manager: "npm" }
-        const hash_now = toHashString(await Crypto.crypto.subtle.digest("SHA-256", new TextEncoder().encode(JSON.stringify(json))), "hex")
+        const hash_now = toHashString(
+            await Crypto.crypto.subtle.digest(
+                "SHA-256",
+                new TextEncoder().encode(JSON.stringify(json)),
+            ),
+            "hex",
+        )
         const cache = await readCache(json.name, hash_now, config)
-        if(cache) {
+        if (cache) {
             modules.add(cache)
             continue
         }
@@ -31,12 +43,22 @@ export async function crawlNodeModules(root: string, config: Config): Promise<Mo
     return [...modules]
 }
 
-async function readCache(name: string, hash_now: string, config: Config): Promise<Module | undefined> {
-    if(!config.cache) return undefined
+async function readCache(
+    name: string,
+    hash_now: string,
+    config: Config,
+): Promise<Module | undefined> {
+    if (!config.cache) return undefined
     try {
-        const cache = JSON.parse(await Deno.readTextFile(Path.join(Path.join(config.cache, "npm"), `${name}.json`))) as NpmCache
-        if (cache.package_json_md5 !== hash_now){
-            Deno.remove(Path.join(Path.join(config.cache, "npm"), `${name}.json`))
+        const cache = JSON.parse(
+            await Deno.readTextFile(
+                Path.join(Path.join(config.cache, "npm"), `${name}.json`),
+            ),
+        ) as NpmCache
+        if (cache.package_json_md5 !== hash_now) {
+            Deno.remove(
+                Path.join(Path.join(config.cache, "npm"), `${name}.json`),
+            )
             return undefined
         }
         return cache.mod
@@ -45,8 +67,17 @@ async function readCache(name: string, hash_now: string, config: Config): Promis
     }
 }
 
-async function writeCache(name: string, hash_now: string, mod: Module, config: Config): Promise<void> {
-    if(!config.cache) return
+async function writeCache(
+    name: string,
+    hash_now: string,
+    mod: Module,
+    config: Config,
+): Promise<void> {
+    if (!config.cache) return
     const cache: NpmCache = { package_json_md5: hash_now, mod: mod }
-    await Deno.writeTextFile(Path.join(Path.join(config.cache, "npm"), `${name}.json`), JSON.stringify(cache), {create: true})
+    await Deno.writeTextFile(
+        Path.join(Path.join(config.cache, "npm"), `${name}.json`),
+        JSON.stringify(cache),
+        { create: true },
+    )
 }
