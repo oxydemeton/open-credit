e94c6e219815e32381d3081e91e269dc9d2ebce5
M B <mablasteam@gmail.com>
2023-04-01 23:05:09 +0200

    simplified directory deletion

--- src/cli/ClearCache.ts
+++ src/cli/ClearCache.ts
@@ -5,17 +5,6 @@ export async function clearCache(conf: Config) {
     const cache = conf.cache
 
     if (cache) {
-        for (const i in conf.managers) {
-            const managerCache = Path.join(cache, conf.managers[i])
-
-            try {
-                console.log(managerCache)
-
-                await Deno.remove(managerCache, { recursive: true })
-                console.log("Cleared cache for " + conf.managers[i])
-            } catch (e) {
-                console.log("Could not clear cache for " + conf.managers[i])
-            }
-        }
+        Deno.removeSync(cache, { recursive: true })
     }
 }
