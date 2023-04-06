f11316d1466263f8e523202dd7e0548fbb8659e8
M B <mablasteam@gmail.com>
2023-04-07 00:14:48 +0200

    add credit yaml

--- src/Generators/Json.ts
+++ src/Generators/Json.ts
@@ -7,10 +7,12 @@ export function generateJson(modules: Set<Module>, pretty = false): string {
     split.cargo = filterDuplicates(split.cargo)
     split.npm = filterDuplicates(split.npm)
     split.deno = filterDuplicates(split.deno)
+    split.credit_yaml = filterDuplicates(split.credit_yaml)
     const filtered = {
         cargo: split.cargo.length > 0 ? split.cargo : undefined,
         npm: split.npm.length > 0 ? split.npm : undefined,
         deno: split.deno.length > 0 ? split.deno : undefined,
+        yaml: split.credit_yaml.length > 0 ? split.credit_yaml : undefined,
     }
     return JSON.stringify(filtered, undefined, pretty ? 4 : undefined)
 }
